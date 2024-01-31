60ee2c381aaa43c37ed43d08b0ab50f76a9a5147
Wintrack <theo.garnier@epitech.eu>
2024-01-30 23:05:49 -0500

    [+] Adding first validation of data with Pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      email: dto.email,
+      typeOfemail: typeof dto.email,
+      password: dto.password,
+      typeOfpassword: typeof dto.password,
+    });
     return this.authservice.signup();
   }
 
